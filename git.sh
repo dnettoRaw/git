2bc490117028d6624b3e5e70c6c048efd505823a
Daniel NETTO <dnetto@e2r2p7.42.fr>
2017-10-21 22:40:33 +0200

    mise a jour auto v2  at [21-10-22h40m]

--- git.sh
+++ git.sh
@@ -11,22 +11,18 @@ pth_l=`dirname $0`
 
 ######################## Functions ###############################
 function f_echo { if [ $1 -eq $on ] ; then echo $2 ;fi ;}
-function f_error { echo "ERROR !! \n  [$r $1 $w] -h pour page de help."; exit ;}
+function f_error { echo "ERROR !! \n  [$r $1 $w] -h pour page de help."; }
 function f_start 
 {
-	echo "===========================\nconfigurations:"
 	while [ 1 ] ; do
 		#git pull 
-		if [ -f login ] ; then
-			git config --global user.email `cat $pth_l/login | grep Mail | cut -d ' ' -f2-`
-			git config --global user.name `cat $pth_l/login | grep Name | cut -d ' ' -f2-`
-			echo "===========================\n"
-			break 
-		else 
-			read -p "mail: " MAIL
-			read -p "Nom: " NOM
-		   	echo "mail: $MAIL\nNom: $NOM" > $pth_l/login
-		fi
+	if [ -f login ] ; then
+		git config --global user.email `cat $pth_l/login | grep Mail | cut -d ' ' -f2-`
+		git config --global user.name `cat $pth_l/login | grep Name | cut -d ' ' -f2-`
+		break 
+	else 
+		read -p "mail:" MAIL ; read -p "Nom:" NAME ; echo "mail: $MAIL\nNom: $NOM" > $pth_l/login
+	fi
 	done
 }
 function f_help
@@ -41,14 +37,17 @@ function f_help
 
 
 ######################## code ######################################
-	f_start
-	if [ -s "$1" ] ; then f_error "NO ARGS" ; fi
-	if [ "$1" = "-h" ] || [ "$1" = "-H" ] ; then f_help ; fi 
-	if [ "$1" = "-a" ] ; then if [ -s "$2" ] ; then git add "." ; else git add "$2" ; fi ; fi
-	if [ "$1" = "-c" ] ; then if [ -s "$2" ] ; then f_error "NO COMMIT || DEFAUT= $f\"git sh - $USER - $time - $host\"$w" ; git commit -m "git sh - $USER - $time - $host" ; else git commit -m "$2" ; fi ; fi
-	if [ "$1" = "-p" ] ; then git push ; fi
-	if [ "$1" = "-pull" ] ; then git pull ; fi
-	if [ ${1:0:1} != "-" ] ; then echo "$f-=- git add . -=-$w" ; git add "." ; echo "\n$f-=- commit -m \"$1\" -=-$w\n" ; git commit -m "$1  at [$time]" ; echo "\n$f-=- git push -=-$w\n" ; git push ; fi
+f_start
+while [ 1 ] ; do
+	if [ -s "$1" ] ; then f_error "NO ARGS" ; break ; fi
+	if [ "$1" = "-h" ] || [ "$1" = "-H" ] ; then f_help ; break ; fi 
+	if [ "$1" = "-a" ] ; then if [ -s "$2" ] ; then git add "." ; else git add "$2" ; fi ; break ; fi
+	if [ "$1" = "-c" ] ; then if [ -s "$2" ] ; then f_error "NO COMMIT || DEFAUT= $f\"git sh - $USER - $time - $host\"$w" ; git commit -m "git sh - $USER - $time - $host" ; else git commit -m "$2" ; fi ; break ; fi
+	if [ "$1" = "-p" ] ; then git push ; break ; fi
+	if [ "$1" = "-pull" ] ; then git pull ; break ; fi
+	if [ ${1:0:1} != "-" ] ; then echo "$f-=- git add . -=-$w" ; git add "." ; echo "\n$f-=- commit -m \"$1\" -=-$w\n" ; git commit -m "$1  at [$time]" ; echo "\n$f-=- git push -=-$w\n" ; git push ; break ; fi
 	f_error "argument non valide"
 	#------- by dnetto -------------------------------------
+	break
+done
 ######################################################################
