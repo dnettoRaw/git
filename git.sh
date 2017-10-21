015b92f2cf844f914002840cd1108c67ee00f129
dnetto42 <dnetto@student.42.fr>
2017-10-21 23:48:34 +0200

    last maj  at [21-10-23h48m]

--- git.sh
+++ git.sh
@@ -12,27 +12,23 @@ pth_l=`dirname $0`
 function f_echo { if [ $1 -eq $on ] ; then echo $2 ;fi ;}
 function f_error { echo "ERROR !! \n  [$r $1 $w] -h pour page de help."; }
 function f_start 
-{
+{ 
 	while [ 1 ] ; do
 		#git pull 
-	if [ -f login ] ; then
-		mailf=`cat $pth_l/login | grep Mail | cut -d ' ' -f2-`;nomf=`cat $pth_l/login | grep Nom | cut -d ' ' -f2-`
-		git config --global user.email "$mailf"
-		git config --global user.name "$nomf"
-		echo ".$nomf.$mailf."
-		break 
-	else 
-		read -p "mail : " MAIL ; read -p "Nom : " NAME ; echo "Mail: $MAIL\nNom: $NAME" > $pth_l/login
-	fi
+	if [ -f "$pth_l/login" ] ; then mailf=`cat $pth_l/login | grep Mail | cut -d ' ' -f2-`; nomf=`cat $pth_l/login | grep Nom | cut -d ' ' -f2-` ; git config --global user.email "$mailf" ; git config --global user.name "$nomf" ; break
+   	else echo "le login et le mail du github, si tu veux pas etre identifie, metre ex: \nmail : ba@co.fr\nlogin : baba\n"; read -p "mail : " MAIL ; read -p "login : " NAME ; echo "Mail: $MAIL\nNom: $NAME" > $pth_l/login ; fi
 	done
 }
 function f_help
 {
 	echo "git sh -help:";echo "\t-h -H\thelp meu";echo "\nusage:"
-	echo "\tgitsh [-flag] [\"commit\"]"; echo "\nflags:\n\tsans flags et un commit il \n\tfera: git add . ; git -m commit [\$commentaire]; git push\n"
+	echo "\tgitsh [-flag] [\"commit\"]"; echo "\nflags:\n\tsans flags et un commit \n\til fera:$f git add . ; git -m commit [\$commentaire]; git push\n$w"
 	echo "\t-a\tgit add [\$argument1]\n\t\t  s'il n'as pas d'args ils fera:[ git add . ] Par defaut"
 	echo "\t-c\tgit commit -m\n\t\t  msg defaut : $f\"git sh - $USER - $time - $host\"$w"
-	echo "\t-p\tgit push";echo "\t-pull\tgit pull";echo "\t-alias\tset le_nom_passe=sh $PWD/$0$r(beta)$w\n\t\t  ou defaut : gitsh=sh $PWD/$0 $r(beta)$w"
+	echo "\t-p\tgit push";echo "\t-pull\tgit pull"
+	#echo "\t-alias\tset le_nom_passe=sh $PWD/$0$r(beta)$w\n\t\t  ou defaut : gitsh=sh $PWD/$0 $r(beta)$w"
+	echo "\t-s\tset alias in .zshrc\n\t\t  defaut : $f alias git_sh=\"sh $pth_l$0v\"$w\n\t\t$y  !!attention, pas de verif effectue, eviter de salir zshrc avec des multiples -s!!$w"
+	echo "\t-r\treset user.login && user.email"
 }
 ####################################################################
 
@@ -46,6 +42,8 @@ while [ 1 ] ; do
 	if [ "$1" = "-c" ] ; then if [ -s "$2" ] ; then f_error "NO COMMIT || DEFAUT= $f\"git sh - $USER - $time - $host\"$w" ; git commit -m "git sh - $USER - $time - $host" ; else git commit -m "$2" ; fi ; break ; fi
 	if [ "$1" = "-p" ] ; then git push ; break ; fi
 	if [ "$1" = "-pull" ] ; then git pull ; break ; fi
+	if [ "$1" = "-s" ] ; then echo "alias git_sh=\"sh $pth_l$0v\"">> ~/.zshrc ; echo "alias set [${g}ok${w}]"; break ;fi
+	if [ "$1" = "-r" ] ; then read -p "new mail : " MAIL ; read -p "new login : " NAME ; echo "Mail: $MAIL\nNom: $NAME" > $pth_l/login ; break ;fi  
 	if [ ${1:0:1} != "-" ] ; then echo "$f-=- git add . -=-$w" ; git add "." ; echo "\n$f-=- commit -m \"$1\" -=-$w\n" ; git commit -m "$1  at [$time]" ; echo "\n$f-=- git push -=-$w\n" ; git push ; break ; fi
 	f_error "argument non valide"
 	#------- by dnetto -------------------------------------
