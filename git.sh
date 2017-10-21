e47def6c65f8facc5a17b52fa419db392f12694f
Daniel <dnetto42@users.noreply.github.com>
2017-10-21 21:41:43 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -5,12 +5,27 @@
 time=`date '+%d-%m-%Hh%Mm'`; if [ -z "$host" ] ;then host=`uname -n`;fi
 w="\033[0m";r="\033[31m";g="\033[32m";y="\033[33m";f="\033[1m"
 b="\033[34m";v="\033[35m";c="\033[36m";s="\033[37m";on=1;off=0
+pth_l=`dirname $0`
 ##################################################################
 
 
 ######################## Functions ###############################
 function f_echo { if [ $1 -eq $on ] ; then echo $2 ;fi ;}
 function f_error { echo "ERROR !! \n  [$r $1 $w] -h pour page de help."; }
+function f_start 
+{
+	echo "===========================\nconfigurations:"
+	while [ 1 ] ; do
+	git pull 
+	if [ -f login ] ; then
+		git config --global user.email `cat $pth_l/login | grep Mail | cut -d ' ' -f2-`
+		git config --global user.name `cat $pth_l/login | grep Name | cut -d ' ' -f2-`
+		echo "===========================\n" ; break 
+	else 
+		read -p "mail:" MAIL ; read -p "Nom:" NAME ; echo "mail: $MAIL\nNom: $NOM" > $pth_l/login
+	fi
+	done
+}
 function f_help
 {
 	echo "git sh -help:";echo "\t-h -H\thelp meu";echo "\nusage:"
